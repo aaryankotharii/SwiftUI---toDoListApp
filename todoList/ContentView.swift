40f064c7b1ada0da6eb28e9dc56e397416b35a35
aaryankotharii <aaryan.kothari@gmail.com>
2020-04-13 00:41:35 +0530

    App complete

--- todoList/ContentView.swift
+++ todoList/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @FetchRequest(fetchRequest: ToDoItem.getAllToDoItems()) var toDoItems : FetchedResults<ToDoItem>
      
     @State private var newToDoItem = ""
+    
     var body: some View {
         NavigationView{
             List{
